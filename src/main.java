a163398887d22ae9409964d004d16e77fcbf15d8
AbaiAlex <33180187+AbaiAlex@users.noreply.github.com>
2020-04-24 21:34:00 +0200

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -6,9 +6,15 @@ public class main {
         char[][] Board = new char[5][5];
         int row = 0;
         int col = 0;
+        int countAItry =0;
         Random rand = new Random();
         int Round = rand.nextInt(2);
-        System.out.println(Round);
+        if(Round==0){
+            System.out.print("AI start the game\n" );
+
+        }else{
+            System.out.print("Player start the game\n" );
+        }
         int playerGetColor=rand.nextInt(2);
         char Player=' ';
         char AI=' ';
@@ -16,9 +22,13 @@ public class main {
         {
             Player='B';
             AI='R';
+            System.out.print("Player color is Blue(B)\n" );
+            System.out.print("AI color is Red(R)\n" );
         }else{
             Player='R';
             AI='B';
+            System.out.print("Player color is Red(R)\n" );
+            System.out.print("AI color is Blue(B)\n" );
         }
         try (Scanner scanner = new Scanner(System.in)) {
             for (int i = 0; i < Board.length; i++) {
@@ -26,54 +36,94 @@ public class main {
                     Board[i][j] = ' ';
                 }
             }
-            boolean noWinGame = true;
-            while (noWinGame) {
+            boolean noOneWinTheGameYet = true;
+            while (noOneWinTheGameYet) {
                 System.out.println("------------");
                 for (int i = 0; i < Board.length; i++) {
                     System.out.printf("| %c %c %c %c %c |\n", Board[i][0], Board[i][1], Board[i][2],Board[i][3],Board[i][4]);
                 }
                 System.out.println("------------");
-                noWinGame = gamestate(Board);
-                while (noWinGame) {
+                noOneWinTheGameYet = gamestate(Board);
+                while (noOneWinTheGameYet) {
                     if (Round % 2 != 0){
                         System.out.print("Player Round\n");
-                        System.out.print("Sor és oszlop: ");
+                        System.out.print("Enter row and column values: ");
 
 
-                    String[] rowAndCol = scanner.nextLine().split(" ");
-                    try {
-                        row = Integer.parseInt(rowAndCol[0]) - 1;
-                        col = Integer.parseInt(rowAndCol[1]) - 1;
-                    } catch (Exception e) {
-                        System.out.println("A sor és oszlop értékek csak számok lehetnek!");
-                        continue;
-                    }
-                    if (row + 1 > 5 || row + 1 < 1 || col + 1 > 5 || col + 1 < 1) {
-                        System.out.println("A koordináták csak 1-től 5-ig lehet megadni!");
-                        continue;
-                    }
-                    if (Board[row][col] == ' ') {
-                        Board[row][col] = Player;
-                        Round++;
-                        break;
-                    } else {
-                        System.out.println("Ez a cella nem üres!");
-                        continue;
-                    }
-                }else{
-                        System.out.print("AI Round\n");
+                        String[] rowAndCol = scanner.nextLine().split(" ");
+                        try {
+                            row = Integer.parseInt(rowAndCol[0]) - 1;
+                            col = Integer.parseInt(rowAndCol[1]) - 1;
+                        } catch (Exception e) {
+                            System.out.println("Row and column values can only be numbers!");
+                            continue;
+                        }
+                        if (row + 1 > 5 || row + 1 < 1 || col + 1 > 5 || col + 1 < 1) {
+                            System.out.println("Coordinates can only be entered from 1 to 5!");
+                            continue;
+                        }
+                        if (Board[row][col] == ' ') {
+                            Board[row][col] = Player;
+                            Round++;
+                            break;
+                        } else {
+                            System.out.println("This cell is not empty!");
+                            continue;
+                        }
+                    }else{
+
                         int i =rand.nextInt(5);
                         int j =rand.nextInt(5);
                         if( Board[i][j]==' '){
                             Board[i][j] = AI;
-                            Round++;
-                            break;
+                            if(gamestate(Board)==false){
+                                Board[i][j] = ' ';
+                                countAItry++;
+                                if(countAItry>=1000)
+                                {
+                                    System.out.print("AI Round\n");
+                                    for (int x=0; x<5;x++){
+                                        for (int y=0; y < 5;y++) {
+                                            if (Board[x][y] == ' ') {
+                                                Board[x][y] = AI;
+                                                Round++;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                continue;
+                            }else{
+                                System.out.print("AI Round\n");
+                                Round++;
+                                break;
+                            }
                         }else {
                             continue;
                         }
                     }
 
                 }
+            }
+        }
+        if(gamestate(Board)==false)
+        {
+            if (Round%2!=0) {
+                System.out.println("-----------");
+                System.out.println("| \\ - - - / |");
+                System.out.println("| -\\ - - /- |");
+                System.out.println("| P l a y e r |");
+                System.out.println("| -/W I N\\- |");
+                System.out.println("| / - - - \\ |");
+                System.out.println("-----------");
+            }else {
+                System.out.println("-----------");
+                System.out.println("|\\ - - - /|");
+                System.out.println("|-\\ - - /-|");
+                System.out.println("|   A I   |");
+                System.out.println("|-/W I N\\-|");
+                System.out.println("|/ - - - \\|");
+                System.out.println("-----------");
             }
         }
     }
@@ -252,7 +302,7 @@ public class main {
                         diagCountRed++;
                         if (diagCountRed>=3){
                             win++;
-                            winnerChar='B';
+                           winnerChar='B';
                         }
                     }else if (Board[i][j] == 'B') {
                         diagCountBlue++;
@@ -318,11 +368,9 @@ public class main {
                 }
             }
         }
-        diagCountBlue=0;
-        diagCountRed=0;
 
         if (win >= 1) {
-            if (winnerChar=='B') {
+            /*if (winnerChar=='B') {
                 System.out.println("-----------");
                 System.out.println("|\\ - - - /|");
                 System.out.println("|-\\ - - /-|");
@@ -338,11 +386,9 @@ public class main {
                 System.out.println("|-/W I N\\-|");
                 System.out.println("|/ - - - \\|");
                 System.out.println("-----------");
-            }
+            }*/
             return false;
-
         }
-
         return true;
     }
 }
